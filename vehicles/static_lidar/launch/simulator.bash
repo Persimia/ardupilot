d5b77622b831c6f8ff4d9510abeb4d766621c6b6
Arjun <krishnan.arjun@gatech.edu>
2024-09-18 10:34:49 -0400

    Fixes "simulator.bash" for vehicles/static_lidar

--- vehicles/static_lidar/launch/simulator.bash
+++ vehicles/static_lidar/launch/simulator.bash
@@ -10,6 +10,6 @@ else
     exit 1
 fi
 (trap 'kill 0' SIGINT;
- ros2 run ros_gz_bridge parameter_bridge --ros-args -p config_file:= $PWD/config/static_lidar.yaml &
- ros2 run micro_ros_agent micro_ros_agent udp4 --middleware dds --verbose 4 --port 2019 --ros-args -r __node:=micro_ros_agent -r __ns:=/ &
+ ros2 run ros_gz_bridge parameter_bridge --ros-args -p config_file:= $PWD/config/static_lidar.yaml &\
+ ros2 run micro_ros_agent micro_ros_agent udp4 --middleware dds --verbose 4 --port 2019 --ros-args -r __node:=micro_ros_agent -r __ns:=/ &\
  gz sim -v4 -r $worldfile)
